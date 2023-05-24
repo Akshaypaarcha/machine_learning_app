4fd22d8ed2d77ff89e374a317397d054ffbaa357
Akshaypaarcha <112705464+Akshaypaarcha@users.noreply.github.com>
2023-05-24 14:30:48 +0530

    Update mlapp.py

--- mlapp.py
+++ mlapp.py
@@ -12,7 +12,7 @@ import pycaret
 from pycaret.classification import setup, compare_models, pull, save_model, ClassificationExperiment
 from pycaret.regression import setup, compare_models, pull, save_model, RegressionExperiment
 
-st.image("MachineLearning.gif")
+st.image("MachineLearning.jpg")
 st.title("Machine Learning App using PyCaret")
 
 if os.path.exists("sourcev.csv"):
