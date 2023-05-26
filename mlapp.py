ee18dcf9409839efba780fd26227bdd004aee087
AKSHAY PAARCHA <112705464+Akshaypaarcha@users.noreply.github.com>
2023-05-26 12:33:49 +0530

    Update mlapp.py

--- mlapp.py
+++ mlapp.py
@@ -20,8 +20,7 @@ if os.path.exists("sourcev.csv"):
 
 with st.sidebar:
     st.header("Welcome to Machine Learning Model")
-    st.subheader("This model helpful for explore and analyze")
-    st.subheader("The user import the data. select the parameter, the model can read the dataset, explore and analyzes, and so result. Users can download the result")
+    st.subheader("This model helpful for explore and analyze"),("The user import the data. select the parameter, the model can read the dataset, explore and analyzes, and so result. Users can download the result")
 ")
     st.caption("Choose your parameters here: ")
     choose=st.radio(":computer:",["Dataset","Explore","Train","Download"])
